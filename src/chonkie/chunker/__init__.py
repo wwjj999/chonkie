0afa633f802e10aca178a7aca32f0959200d8359
bhavnicksm <bhavnicksm@gmail.com>
2024-12-27 15:44:47 +0530

    Add RecursiveChunker and associated dataclasses to __init__

--- src/chonkie/chunker/__init__.py
+++ src/chonkie/chunker/__init__.py
@@ -1,15 +1,17 @@
 """Module for chunkers."""
 
 from .base import BaseChunker
+from .late import LateChunker
+from .recursive import RecursiveChunker
 from .sdpm import SDPMChunker
 from .semantic import SemanticChunker
 from .sentence import SentenceChunker
 from .token import TokenChunker
 from .word import WordChunker
-from .late import LateChunker
 
 __all__ = [
     "BaseChunker",
+    "RecursiveChunker",
     "TokenChunker",
     "WordChunker",
     "SentenceChunker",
